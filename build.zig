37ec6352348a50e9b75ba19394ffa218ca524054
hasanpasha <bashahsn22@gmail.com>
2024-05-13 18:57:43 +0000

    link c and mpv with test binary

--- build.zig
+++ build.zig
@@ -89,6 +89,9 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
 
+    exe_unit_tests.linkLibC();
+    exe_unit_tests.linkSystemLibrary("mpv");
+
     const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
 
     // Similar to creating the run step earlier, this exposes a `test` step to
