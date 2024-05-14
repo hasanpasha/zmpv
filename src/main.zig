5438d1efb0a3d61b2e29c797f56fa895390f5dc9
hasanpasha <bashahsn22@gmail.com>
2024-05-14 21:29:52 +0000

    use command screenshot-raw to check if ByteArray is correct

--- src/main.zig
+++ src/main.zig
@@ -34,12 +34,14 @@ pub fn main() !void {
 
     try mpv.loadfile("sample.mp4", .{});
 
-    var args = [_][]const u8{ "cycle", "fullscreen" };
-    const result = try mpv.command_ret(&args);
-    std.log.debug("[cycle command result] {}", .{result});
+    // var args = [_][]const u8{"screenshot-raw"};
+    // const result = try mpv.command_ret(&args);
+    // std.log.debug("[screenshot-raw result] {}", .{result});
 
     // var args = [_][]const u8{ "cycle", "pause" };
-    // try mpv.command_async(0, &args);
+    // var load_args = [_][]const u8{ "loadfile", "sample.mp4" };
+    // try mpv.command_async(6969, &load_args);
+    // mpv.abort_async_command(6969);
 
     try mpv.request_log_messages(.None);
 
@@ -58,7 +60,7 @@ pub fn main() !void {
     // const fullscreen_status = try mpv.get_property("fullscreen", .String);
     // std.log.debug("[fullscreen]: {s}", .{fullscreen_status.String});
 
-    // var time_pos_not_changed = true;
+    var time_pos_not_changed = true;
     while (true) {
         const event = try mpv.wait_event(10000);
         switch (event.event_id) {
@@ -66,14 +68,18 @@ pub fn main() !void {
             .CommandReply => {
                 std.log.debug("[event] {}", .{event.data.CommandReply.result});
             },
+            // .FileLoaded => {
+            //     var args = [_][]const u8{"screenshot-raw"};
+            //     try mpv.command(&args);
+            // },
             .Hook => {
                 std.log.debug("[event] hook {}", .{event.data});
 
                 std.log.debug("HANDLING {s} hook", .{event.data.Hook.name});
 
-                for (0..100) |i| {
-                    std.log.debug("DOING WORK {}", .{i});
-                }
+                // for (0..100) |i| {
+                //     std.log.debug("DOING WORK {}", .{i});
+                // }
 
                 std.log.debug("CONTINUING", .{});
                 try mpv.hook_continue(event.data.Hook.id);
@@ -82,36 +88,45 @@ pub fn main() !void {
             //     // const filename = try mpv.get_property("filename", .Node);
             //     // std.log.debug("[filename]: {s}", .{filename.Node.String});
             // },
-            // .PropertyChange => {
-            //     const property_change = event.data.PropertyChange;
-            //     std.log.debug("[property_change] name={s} value={any}", .{ property_change.name, property_change.data });
-            //     std.debug.print("[propertyChange] name={s}, {?}\n", .{ property_change.name, property_change.data });
-            //     //     const playlist = try mpv.get_property("playlist", .Node);
-            //     //     std.log.debug("[playlist]: {any}", .{playlist.Node});
-            //     //     // const title_osd = try mpv.get_property("title", .String);
-            //     //     // std.log.debug("[title_osd] {}\n", .{title_osd});
+            .PropertyChange => {
+                const property_change = event.data.PropertyChange;
+                //     std.log.debug("[property_change] name={s} value={any}", .{ property_change.name, property_change.data });
+                //     std.debug.print("[propertyChange] name={s}, {?}\n", .{ property_change.name, property_change.data });
+                //     //     const playlist = try mpv.get_property("playlist", .Node);
+                //     //     std.log.debug("[playlist]: {any}", .{playlist.Node});
+                //     //     // const title_osd = try mpv.get_property("title", .String);
+                //     //     // std.log.debug("[title_osd] {}\n", .{title_osd});
 
-            //     // if (std.mem.eql(u8, property_change.name, "time-pos")) {
-            //     //     switch (property_change.data) {
-            //     //         .Double => |num| {
-            //     //             std.log.debug("TIME-POS {}", .{num});
-            //     //             if (num >= 3.14 and time_pos_not_changed) {
-            //     //                 time_pos_not_changed = false;
-            //     //                 try mpv.set_property("time-pos", .Double, .{ .Double = num * 2 });
-            //     //             }
-            //     //         },
-            //     //         else => {},
-            //     //     }
-            //     // }
-            // },
+                if (std.mem.eql(u8, property_change.name, "time-pos")) {
+                    switch (property_change.data) {
+                        .Double => |num| {
+                            // std.log.debug("TIME-POS {}", .{num});
+                            if (num >= 3.14 and time_pos_not_changed) {
+                                time_pos_not_changed = false;
+                                // try mpv.set_property("time-pos", .Double, .{ .Double = num * 2 });
+                                var args = [_][]const u8{"screenshot-raw"};
+                                const data = try mpv.command_ret(&args);
+                                // std.log.debug("[screenshot] {?}", .{data.NodeMap.get("data")});
+                                std.log.debug("[screenshot] format={s}", .{data.NodeMap.get("format").?.String});
+                                std.log.debug("[screenshot] w={}", .{data.NodeMap.get("w").?.INT64});
+                                std.log.debug("[screenshot] h={}", .{data.NodeMap.get("h").?.INT64});
+                                std.log.debug("[screenshot] stride={}", .{data.NodeMap.get("stride").?.INT64});
+                                // var stop_cmd = [_][]const u8{"quit"};
+                                // try mpv.command(&stop_cmd);
+                            }
+                        },
+                        else => {},
+                    }
+                }
+            },
             .LogMessage => {
                 const log = event.data.LogMessage;
                 std.log.debug("[log] {s} \"{s}\"", .{ log.level, log.text });
             },
-            // .EndFile => {
-            //     const endfile = event.data.?.EndFile;
-            //     std.debug.print("[endfile] {any}\n", .{endfile});
-            // },
+            .EndFile => {
+                const endfile = event.data.EndFile;
+                std.debug.print("[endfile] {any}\n", .{endfile});
+            },
             else => {},
         }
     }
