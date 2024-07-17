2bdd9af34daeb678878be42de5b273ab97a0bd96
hasanpasha <bashahsn22@gmail.com>
2024-07-17 19:11:13 +0300

    `MpvEventIterator` code refactor

--- src/MpvEventIterator.zig
+++ src/MpvEventIterator.zig
@@ -8,16 +8,18 @@ pub const MpvEventIteratorWaitFlag = union(enum) {
     NoWait: void,
     IndefiniteWait: void,
     TimedWait: f64,
+
+    pub fn to_c(self: @This()) f64 {
+        return switch (self) {
+            .NoWait => 0,
+            .IndefiniteWait => -1,
+            .TimedWait => |value| value,
+        };
+    }
 };
 
 pub fn next(self: @This()) ?MpvEvent {
-    const timeout: f64 = switch (self.wait_flag) {
-        .NoWait => 0,
-        .IndefiniteWait => -1,
-        .TimedWait => |value| value,
-    };
-
-    const event = self.handle.wait_event(timeout);
+    const event = self.handle.wait_event(self.wait_flag.to_c());
     if (event.event_id == .None) return null;
     return event;
 }
